2609f09c693d1316e99cb7dca39aaa0ae2d3835b
Jose Santos <airamez@gmail.com>
2025-03-25 02:33:26 -0700

    Version 1.0

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -1,5 +1,4 @@
 using System;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Bomb : MonoBehaviour
@@ -7,23 +6,34 @@ public class Bomb : MonoBehaviour
     public float explosionRadius = 5f; // Radius of the explosion
     public LayerMask enemyLayer; // Specify the layer for enemies
     public string enemyName = "Enemy";
+    private AudioSource bombExplosion; 
+    private AudioSource enemyExplosion; 
+
+    void Start () {
+        AudioSource[] audioSources = GetComponents<AudioSource>();
+        bombExplosion = GetComponent<AudioSource>();
+        bombExplosion = audioSources[0];
+        enemyExplosion = audioSources[1]; 
+    }
 
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.name == "Ground" || collision.gameObject.name.StartsWith(enemyName))
         {
+            gameObject.GetComponent<Renderer>().enabled = false;
             Explode();
-            Destroy(gameObject);
         }
     }
 
     void Explode()
     {
+        //bombExplosion.Play();
         Collider[] enemies = Physics.OverlapSphere(transform.position, explosionRadius, enemyLayer);
         foreach (Collider enemy in enemies)
         {
-            Destroy(enemy.gameObject);
+            enemyExplosion.Play();
+            Destroy(enemy.gameObject, enemyExplosion.clip.length);
         }
-        Debug.Log("Boom! Explosion triggered.");
+        Destroy(gameObject, enemyExplosion.clip.length);
     }
 }
