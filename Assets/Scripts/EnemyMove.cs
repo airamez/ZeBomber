9ef6dc2cdc4f8bc03b3d9707d6552c73942c626b
Jose Santos <airamez@gmail.com>
2025-03-25 20:18:17 -0700

    Version 2.0

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -1,18 +1,32 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EnemyMover : MonoBehaviour
 {
     public float moveSpeed = 2f; // Speed of movement
+    public static int pointsLeft = 20;
+    private TextMeshProUGUI pointsLeftText;
+
+    void Start () {
+        GameObject pointsLeftObject = GameObject.Find("PointsLeft");
+        pointsLeftText = pointsLeftObject.GetComponent<TextMeshProUGUI>();
+    }
 
     void Update()
     {
         // Move towards base (0, 0, 0)
         transform.position = Vector3.MoveTowards(transform.position, Vector3.zero, moveSpeed * Time.deltaTime);
 
-        // Too close to base
+        // Got at base
         if (Vector3.Distance(transform.position, Vector3.zero) < 10f)
         {
             Destroy(gameObject);
+            pointsLeft--;
+            pointsLeftText.text = $"Points Left: {pointsLeft}";
+            if (pointsLeft <= 0) {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
         }
     }
 }
